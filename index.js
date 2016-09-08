9d126fa90602a5ddcbf30a4c107fd98c3e039406
Ivan Collado <collado.ip@gmail.com>
2016-09-08 02:27:03 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function iterativeLog(array) {
+  var index = 0;
+  array.forEach(element => {
+    console.log(`${index}: ${element}`)
+    index++;
+  })
+}
+
+function iterate(callback) {
+  var array = [0];
+  array.forEach(callback)
+  return array;
+}
+
+function doToArray(array, callback) {
+  array.forEach(callback)
+}
